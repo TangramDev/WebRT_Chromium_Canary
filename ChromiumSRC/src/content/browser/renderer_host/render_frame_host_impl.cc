6d02dabd4ab0eef0c07de1143957c83145da5f67
tangram <tangramteam@outlook.com>
2022-10-18 16:37:47 +0800

    109.0.5366.1

--- ChromiumSRC/src/content/browser/renderer_host/render_frame_host_impl.cc
+++ ChromiumSRC/src/content/browser/renderer_host/render_frame_host_impl.cc
@@ -1161,7 +1161,7 @@ bool IsWindowManagementGranted(RenderFrameHost* host) {
   DCHECK(permission_controller);
 
   return permission_controller->GetPermissionStatusForCurrentDocument(
-             blink::PermissionType::WINDOW_PLACEMENT, host) ==
+             blink::PermissionType::WINDOW_MANAGEMENT, host) ==
          blink::mojom::PermissionStatus::GRANTED;
 }
 
@@ -7945,7 +7945,7 @@ void RenderFrameHostImpl::DidChangeSrcDoc(
 }
 
 void RenderFrameHostImpl::DidChangeBaseURL(const GURL& base_url) {
-  if (!SiteIsolationPolicy::AreIsolatedSandboxedIframesEnabled())
+  if (!blink::features::IsNewBaseUrlInheritanceBehaviorEnabled())
     return;
 
   // TODO(https://crbug.com/1356658,1366593): consider restricting base URL in
@@ -7954,10 +7954,10 @@ void RenderFrameHostImpl::DidChangeBaseURL(const GURL& base_url) {
 }
 
 const GURL& RenderFrameHostImpl::GetBaseUrl() const {
-  if (!SiteIsolationPolicy::AreIsolatedSandboxedIframesEnabled()) {
+  if (!blink::features::IsNewBaseUrlInheritanceBehaviorEnabled()) {
     NOTREACHED() << __func__
                  << " should only be invoked when the feature "
-                    "IsolateSandboxedIframes is enabled.";
+                    "NewBaseUrlInheritanceBehavioris enabled.";
     return GURL::EmptyGURL();
   }
 
@@ -11787,7 +11787,8 @@ void RenderFrameHost::LogSandboxedIframesIsolationMetrics() {
       sandboxed_rphs.size());
 }
 
-void RenderFrameHostImpl::UpdateIsolatableSandboxedIframeTracking() {
+void RenderFrameHostImpl::UpdateIsolatableSandboxedIframeTracking(
+    NavigationRequest* navigation_request) {
   RoutingIDIsolatableSandboxedIframesSet* oopsifs =
       g_routing_id_isolatable_sandboxed_iframes_set.Pointer();
   GlobalRenderFrameHostId global_id = GetGlobalId();
@@ -11810,8 +11811,9 @@ void RenderFrameHostImpl::UpdateIsolatableSandboxedIframeTracking() {
       // frame, to see if the url can be placed in an OOPSIF, i.e. it's not
       // already isolated because of being cross-site.
       RenderFrameHost* frame_owner = GetParent();
-      if (!frame_owner && frame_tree_node_->opener())
-        frame_owner = frame_tree_node_->opener()->current_frame_host();
+      FrameTreeNode* opener = navigation_request->frame_tree_node()->opener();
+      if (!frame_owner && opener)
+        frame_owner = opener->current_frame_host();
 
       if (!frame_owner) {
         frame_is_isolatable = false;
@@ -12233,7 +12235,7 @@ void RenderFrameHostImpl::DidCommitNewDocument(
 
   accessibility_fatal_error_count_ = 0;
 
-  UpdateIsolatableSandboxedIframeTracking();
+  UpdateIsolatableSandboxedIframeTracking(navigation_request);
 }
 
 // TODO(arthursonzogni): Below, many NavigationRequest's objects are passed from
