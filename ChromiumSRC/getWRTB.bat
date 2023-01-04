653010c2ddbe6bb04f7d34a3f568d0f9b427b49e
Tangram <tangramteam@outlook.com>
2023-01-05 05:48:45 +0800

    support M111

--- ChromiumSRC/getWRTB.bat
+++ ChromiumSRC/getWRTB.bat
@@ -1,2 +1,2 @@
 @echo off
-git fetch origin tag 109.0.%1 && git checkout -B M109.%1 109.0.%1&& gclient sync -D && git cherry-pick Base&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
+git fetch origin tag 111.0.%1 && git checkout -B M111.%1 111.0.%1&& gclient sync -D && git cherry-pick Base&&..\bd %2&&git checkout Base&&git pull&&gclient sync -D
